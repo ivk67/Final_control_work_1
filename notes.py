c85b189306928645ee6c463b37fb86a1a95f52f0
ilya_k <ivk68@list.ru>
2023-06-19 16:05:05 +0300

    finishing touches

--- notes.py
+++ notes.py
@@ -53,20 +53,12 @@ def delete_note():
             return
     print("Заметка с таким ID не найдена.")
 
-# def save_file():
-#     file_name = asksaveasfilename(defaultextension=".json")
-#     if file_name:
-#         with io.open(file_name, "w", encoding="utf-8") as f:
-#             for note in notes:
-#                 f.write(f"{note['id']};{note['timestamp']};{note['title']};{note['body']}\n")
-
 def save_file():
     file_name = asksaveasfilename(defaultextension=".json") 
     if file_name:
         with io.open(file_name, "w", encoding="utf-8") as f:
             json.dump(notes, f, ensure_ascii=False)
-        
-
+       
 def load_file():
     try:
         file_name = askopenfilename(filetypes=[("JSON Files", "*.json")])
